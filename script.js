7b37ed061f38b6e3939f38f6b689861f59012d18
Cristina Recasens <cristina.recasens13@gmail.com>
2025-05-14 11:48:02 +0200

    Update javascript menu

--- script.js
+++ script.js
@@ -48,3 +48,22 @@ function updateActiveLink() {
         }
     }
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("menu-toggle");
+  const nav = document.querySelector("nav");
+  const links = nav.querySelectorAll("a");
+
+  // Toggle menu al hacer clic en el botón
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+
+  // Oculta el menú al hacer clic en cualquier enlace
+  links.forEach(link => {
+    link.addEventListener("click", () => {
+      nav.classList.remove("show");
+    });
+  });
+});
